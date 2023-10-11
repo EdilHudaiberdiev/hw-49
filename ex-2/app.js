a8cc5cee5a4a9df6393027554e0abbb0405e0822
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2023-10-11 22:05:27 +0600

    Ex2 done

--- ex-2/app.js
+++ ex-2/app.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('work test');
+let num = 0;
+let chess = '';
+for (let i = 0; i < 8; i++) {
+    for (let g = 0; g < 8; g++) {
+        if (num % 2 === 0) {
+            chess += '██';
+        }
+        else {
+            chess += '  ';
+        }
+        num++;
+    }
+    num++;
+    chess += '\n';
+}
+console.log(chess);
--- ex-2/app.ts
+++ ex-2/app.ts
@@ -1,1 +1,17 @@
-console.log('work test');
+let num = 0;
+let chess = '';
+
+for (let i = 0; i < 8; i++){
+    for (let g = 0; g < 8; g++){
+        if (num % 2 === 0) {
+            chess += '██';
+        } else {
+            chess += '  ';
+        }
+        num++;
+    }
+    num++;
+    chess += '\n';
+}
+
+console.log(chess);
